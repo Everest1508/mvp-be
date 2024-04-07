2118e76e178a6bc430e3f4d32326708cf2acc147
Everest1508 <riteshmahale15@gmail.com>
2024-04-07 13:06:48 +0530

    cc

--- mvp/settings.py
+++ mvp/settings.py
@@ -43,10 +43,12 @@ INSTALLED_APPS = [
     'events',
     'rest_framework_simplejwt',
     'participants',
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
